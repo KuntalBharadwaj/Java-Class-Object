80b7247c86bf3252a1ef511ffd1ce0f2960eb264
KuntalBharadwaj <kuntalbharadwaj18@gmail.com>
2025-03-10 15:16:50 +0530

    [Kuntal] add: creating a movie ticket booking programme

---
+++ MovieTicket.java
@@ -0,0 +1,26 @@
+class MovieTicket {
+    String movieName;
+    int seatNumber;
+    double price;
+
+    MovieTicket(String movieName, int seatNumber, double price) {
+        this.movieName = movieName;
+        this.seatNumber = seatNumber;
+        this.price = price;
+    }
+
+    void displayTicket() {
+        System.out.println("Movie: " + movieName);
+        System.out.println("Seat Number: " + seatNumber);
+        System.out.println("Price: " + price);
+    }
+
+    public static void main(String[] args) {
+        MovieTicket ticket = new MovieTicket("Avengers", 12, 250);
+        ticket.displayTicket();
+    }
+}
+
+// Movie: Avengers
+// Seat Number: 12
+// Price: 250.0
